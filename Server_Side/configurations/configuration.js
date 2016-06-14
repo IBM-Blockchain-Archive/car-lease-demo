578202978c7be6046c95cb04fa11d2ab87528890
awjh <a.w.j.hurt-13@student.lboro.ac.uk>
2016-06-14 11:03:29 +0100

    added in the fixes made to IBM-Blockchain repo to this

--- Server_Side/configurations/configuration.js
+++ Server_Side/configurations/configuration.js
@@ -15,7 +15,7 @@ config.api_port_internal = 	JSON.parse(process.env.VCAP_SERVICES)["ibm-blockchai
 config.app_url = 'http://'+JSON.parse(process.env.VCAP_APPLICATION)["application_uris"][0];
 
 //Chaincode file locations
-config.vehicle = "https://github.com/jpayne23/testDeployCC/Chaincode_v0601/vehicle_code";
+config.vehicle = "https://github.com/IBM-Blockchain/car-lease-demo/Chaincode/vehicle_code";
 
 
 //Chaincode deployed names
