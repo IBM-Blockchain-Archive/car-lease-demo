2113fd4088e336e3d56a16eafe4d4e9e103300bd
jpayne23 <payne_500@hotmail.com>
2016-07-18 22:39:31 +0100

    Bluemix now needs https for peers

--- Server_Side/configurations/configuration.js
+++ Server_Side/configurations/configuration.js
@@ -7,7 +7,7 @@ config.trace = true;
 config.traceFile = __dirname+'/../logs/app_trace.log';
 
 //IP and port configuration
-config.api_ip = "http://"+JSON.parse(process.env.VCAP_SERVICES)["ibm-blockchain-5-prod"][0]["credentials"]["peers"][0]["api_host"];
+config.api_ip = "https://"+JSON.parse(process.env.VCAP_SERVICES)["ibm-blockchain-5-prod"][0]["credentials"]["peers"][0]["api_host"];
 //When using blockchain on bluemix, api_port_external and api_port_internal will be the same
 config.api_port_external = JSON.parse(process.env.VCAP_SERVICES)["ibm-blockchain-5-prod"][0]["credentials"]["peers"][0]["api_port"]; //"3000";		//port number used when calling api from outside of the vagrant environment
 config.api_port_internal = 	JSON.parse(process.env.VCAP_SERVICES)["ibm-blockchain-5-prod"][0]["credentials"]["peers"][0]["api_port"]; //"5000";		//port number used when calling api from inside vagrant environment - generally used for chaincode calling out to api
