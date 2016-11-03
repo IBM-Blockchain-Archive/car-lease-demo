bd8248b18afd254db3105afd80d304f3f75ae7a2
Liam Grace <liam.grace@uk.ibm.com>
2016-11-03 10:53:38 +0000

    updated start script

--- Scripts/start.sh
+++ Scripts/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 rm -r /tmp/keyValStore;
+# Tag the latest version of fabric-baseimage
+docker pull hyperledger/fabric-baseimage:x86_64-0.1.0
+docker tag hyperledger/fabric-baseimage:x86_64-0.1.0 hyperledger/fabric-baseimage:latest
 cd ../Demo;
 docker-compose kill;
 docker-compose down;
