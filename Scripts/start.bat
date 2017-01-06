ff9b2454f997d8ce3b47412a21d4958009fd0de9
badr bellaj <bellaj.badr@gmail.com>
2017-01-06 17:34:08 +0000

    Synch with original repos

--- Scripts/start.bat
+++ Scripts/start.bat
@@ -1,14 +1,15 @@
+ 
 @echo off
 set DIR=%~dp0..
 cd %DIR%
-
+:: If the keyValStore exits, remove it
 IF EXIST keyValStore  (
 rmdir /s /q keyValStore
 )
   
  ::Tag the latest version of fabric-baseimage
-:: docker pull hyperledger/fabric-baseimage:x86_64-0.2.2
- ::docker tag hyperledger/fabric-baseimage:x86_64-0.2.2  hyperledger/fabric-baseimage:latest
+docker pull hyperledger/fabric-baseimage:x86_64-0.1.0
+docker tag hyperledger/fabric-baseimage:x86_64-0.1.0  hyperledger/fabric-baseimage:latest
 
 :: Clean up old docker containers
 docker-compose -f %cd%\docker-compose.yml kill
